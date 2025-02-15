8249ae5bce8302488f45a4982040ce32d348c70a
BugHunter-X <askzenai@gmail.com>
2025-02-15 14:46:04 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,10 @@
+function printNumber(num: number): void {
+  console.log(num);
+}
+
+function printAnything(arg: any): void {
+  console.log(arg);
+}
+
+printNumber(123); // Correct usage
+printAnything("hello"); // Correct usage
